e26305de6dd87ccbeb9dd87cfbf1113091a23bde
kenzo101-codeit <kenzo101studios@gmail.com>
2026-01-04 20:50:50 -0300

    Delete fabric directory

--- fabric/src/main/java/me/autobot/elytramace/ElytraMace.java
+++
@@ -1,9 +0,0 @@
-package me.autobot.elytramace;
-
-import net.fabricmc.api.ModInitializer;
-
-public class ElytraMace implements ModInitializer {
-    @Override
-    public void onInitialize() {}
-}
-
