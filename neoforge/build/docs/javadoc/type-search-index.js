41c5bd2de7e844ea67ede3c0fb3b4340ebfd05d4
kenzo101-codeit <kenzo101studios@gmail.com>
2026-01-05 12:17:41 -0300

    added

---
+++ neoforge/build/docs/javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"me.autobot.elytramace.mixin","l":"MixinMultiPlayerGameMode_MaceAttack"}];updateSearchResults();
