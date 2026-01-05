f707c782a5fbc581c1b72d9f576fea67df3b7157
kenzo101-codeit <kenzo101studios@gmail.com>
2026-01-04 21:02:44 -0300

    Add files via upload

---
+++ fabric/src/main/java/me/autobot/elytramace/ElytraMace.java
@@ -0,0 +1,9 @@
+package me.autobot.elytramace;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ElytraMace implements ModInitializer {
+    @Override
+    public void onInitialize() {}
+}
+
